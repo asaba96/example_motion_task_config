81b69d2f0572e973892bb3f93787265f9efbe1e1
Andrew Saba <sabaandrew@icloud.com>
2019-01-23 16:42:59 -0500

    Improvements (#3)
    * fixes, tested more things, etc.
    
    * fixes and testing

--- scripts/test_action_client.py
+++ scripts/test_action_client.py
@@ -7,14 +7,40 @@ from robot_motions_server_ros.msg import TaskRequestAction, TaskRequestGoal
 
 def run(server_name):
     client = actionlib.SimpleActionClient(server_name, TaskRequestAction)
+
+    rospy.loginfo('TaskActionClient: Waiting for server')
     client.wait_for_server()
+    rospy.loginfo('TaskActionClient: server ready')
 
     request = TaskRequestGoal(action_type='test_task')
     client.send_goal_and_wait(request)
 
     rospy.logwarn('Success: {}'.format(client.get_result().success))
 
+    rospy.loginfo('Testing Cancel')
+
+    # test cancel
+    request = TaskRequestGoal(action_type='test_task')
+    client.send_goal(request)
+
+    rospy.sleep(.25)
+
+    client.cancel_goal()
+    rospy.logwarn('Goal Canceled')
+
+    rospy.loginfo('Test Preempt')
+
+    # test preempt
+    request = TaskRequestGoal(action_type='test_task')
+    client.send_goal(request)
+
+    rospy.sleep(.25)
+
+    request = TaskRequestGoal(action_type='test_task', preempt=True)
+    client.send_goal_and_wait(request)
+
 if __name__ == '__main__':
     rospy.init_node('test_action_client')
+    rospy.loginfo('Test Action Client starting up')
     name = rospy.get_param('/task_manager/action_server_name')
     run(name)
